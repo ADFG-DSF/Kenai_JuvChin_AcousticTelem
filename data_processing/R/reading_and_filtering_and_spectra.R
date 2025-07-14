57c267da5cc6f807f8f6f3aebf71b78f40418223
Matt Tyers <matt.tyers@alaska.gov>
2025-07-14 12:49:23 -0800

    annotating filtration test script

--- data_processing/R/reading_and_filtering_and_spectra.R
+++ data_processing/R/reading_and_filtering_and_spectra.R
@@ -23,27 +23,27 @@ readJSATS <- function(filename) {
     # converting DateTime from character 
     raw_data[[i]]$DateTime <- as.POSIXct(raw_data[[i]]$DateTime, 
                                          format="%m/%d/%Y %H:%M:%OS")
+    
     ##### need to figure out how site name is handled??
     # site name seems to have changed between two data tables (only in preamble for first)
     # adding input filename (without file address)
     namesplit <- strsplit(filename, split="/")[[1]]
     raw_data[[i]]$filename <- namesplit[length(namesplit)]
   }
-  # apply filtration function?  write this separately
+  
+  # apply filtration function?  WRITE THIS SEPARATELY
+  
   # write each data table to an external csv file
-  # this is temporary for testing
+  
+  # this is probably temporary for testing
   return(raw_data)
 }
 
-# raw_data <- readJSATS(filename = "flat_data/BeaverCreek012_250527_145301.csv")
-
-raw_data <- readJSATS(filename = "experimentation/BeaverCreek012_250527_145301.csv")
-
-
+raw_data <- readJSATS(filename = "data_processing/test_data/BeaverCreek012_250527_145301.csv")
 # write a separate function to filter, call that in the func above^^
 
 
-## testing zone
+## --------------- testing zone ----------------------
 
 asdf <- raw_data[[2]]
 summary(asdf)
@@ -80,17 +80,28 @@ for(i in 1:length(datetime_diffs_3secs)) plot(datetime_diffs_3secs[[i]], log="y"
 
 
 
-## ...could just sort by paste(TagCode,DateTime, and remove entries without a 3-sec neighbor)
+## TRIAL FILTRATION METHOD:
+## keep only entries for which another entry exists with 
+## - the same tag code, and
+## - is 2.5-3.5 seconds away
+
+
+# if we keep this, it will be wrapped in a function and probably called
+# within the data import function readJSATS()
+
+# first sort by TagCode, then by DateTime
 asdf$orig_row <- 1:nrow(asdf)
 asdf$datetimenum <- as.numeric(asdf$DateTime)
 asdf_sort <- asdf
 asdf_sort$sortby <- paste(asdf_sort$TagCode, asdf_sort$DateTime)
 asdf_sort <- asdf_sort[order(asdf_sort$sortby),]
+
+# initialize logical vectors for conditions in which we want to keep the entry
 keepthese1 <- keepthese2 <- rep(F, nrow(asdf_sort))
 
-# nr <-
+dt <- 0.5  # how close to 3 seconds apart do we require?
 
-dt <- 0.5
+# detecting which rows to keep, then subsetting
 keepthese1[1:(nrow(asdf_sort)-1)] <- 
   (asdf_sort$TagCode[1:(nrow(asdf_sort)-1)] == asdf_sort$TagCode[2:(nrow(asdf_sort))]) &
   (abs(asdf_sort$datetimenum[1:(nrow(asdf_sort)-1)] - asdf_sort$datetimenum[2:(nrow(asdf_sort))]) < (3+dt)) &
@@ -100,27 +111,50 @@ keepthese2[2:(nrow(asdf_sort))] <-
   (abs(asdf_sort$datetimenum[1:(nrow(asdf_sort)-1)] - asdf_sort$datetimenum[2:(nrow(asdf_sort))]) < (3+dt)) &
   (abs(asdf_sort$datetimenum[1:(nrow(asdf_sort)-1)] - asdf_sort$datetimenum[2:(nrow(asdf_sort))]) > (3-dt))
 asdf_sort <- subset(asdf_sort, keepthese1 | keepthese2)
-length(unique(asdf_sort$TagCode))
-table(asdf_sort$TagCode)
-nrow(asdf_sort)
-plot(tapply(asdf_sort$datetimenum, asdf_sort$TagCode, \(x) diff(range(x)))/60/60/24)#, log="y")
+
+
+
+# exploring the output!
+length(unique(asdf_sort$TagCode))  # how many unique tag codes
+table(asdf_sort$TagCode)           # how many entries per tag code
+nrow(asdf_sort)                    # how many total entries, the original was 226k
+
+
+# plotting all records with respect to time.
+# TagCode is on the y-axis, DateTime on the x-axis
+# horizontal line segments link all records with a single TagCode
+# impressions:
+# - after June 9 or so, fish passed by the station relatively quickly
+# - something else was going on in mid May
+# - tag G7205A5CF was maybe tested early on, and deployed in a fish?
+# - tag G72052B5C had over 23k records all by itself!
 par(mfrow=c(1,1))
-# plot(y=as.numeric(as.factor(asdf_sort$TagCode)), x=asdf_sort$DateTime,
-#      col=as.numeric(as.factor(asdf_sort$TagCode)))
 with(asdf_sort, plot(y=as.numeric(as.factor(TagCode)), x=DateTime,
-                     col=as.numeric(as.factor(TagCode))))
+                     col=as.numeric(as.factor(TagCode)),
+                     ylab="", yaxt="n"))
+axis(side=2, las=2, cex.axis=.5,
+     at=1:length(unique(asdf_sort$TagCode)), 
+     labels=levels(as.factor(asdf_sort$TagCode)))
 segments(x0=tapply(asdf_sort$DateTime, asdf_sort$TagCode, min),
          x1=tapply(asdf_sort$DateTime, asdf_sort$TagCode, max),
          y0=1:length(unique(asdf_sort$TagCode)))
 
-# plot(asdf_sort$DateTime, asdf_sort$Tilt)
+# plotting Tilt with respect to DateTime further suggests that late May was something else
 with(asdf_sort, plot(DateTime, Tilt))
 
 
 
 
 
-#### trying a spectral thingy
+## EXPERIMENTING WITH SPECTRAL METHODS
+## This function investigates whether a periodic signal exists, given a sequence of trial frequencies
+## inputs are:
+## - x: a numeric vector, basically as.numeric(DateTime) for an indiv TagCode
+## - lam_min and lam_max: min and max signal PERIODS to consider (in seconds)
+## - n_lam: length of the sequence of trial periods to consider
+##          large number gives better resolution, but is more time-consuming
+## - log_scale: whether to construct the trial sequence on the log scale (the default) or natural
+##              log_scale==TRUE will give a finer resolution at small values
 
 spectra <- function(x, lam_min=1, lam_max=120, n_lam=1000, log_scale=TRUE) { #, lam_trial=exp(seq(from=0, to=log(10), length.out=1000))) {
   if(log_scale) {
@@ -129,30 +163,44 @@ spectra <- function(x, lam_min=1, lam_max=120, n_lam=1000, log_scale=TRUE) { #, 
     lam_trial <- seq(from=lam_min, to=lam_max, length.out=n_lam)
   }
   strength <- pstrength <- rep(NA, length(lam_trial))
-  nbin <- 100
-  delta <- 2
-  alpha_sidak <- 1-(.99^(1/length(lam_trial)))
+  
+  # these might need to be function inputs later on
+  nbin <- 100  # resolution for binning
+  delta <- 2   # number of adjacent bins to include in summation
+  alpha_sidak <- 1-(.99^(1/length(lam_trial))) # Sidak-corrected significance level
+  
   for(i in seq_along(strength)) {
     folded <- x %% lam_trial[i]
+    
+    # # Using Gaussian kernel density
     # thedens <- density(folded, n=nbin)
     # whichmaxes <- (which.max(thedens$y) + (-delta):delta) %% nbin
     # whichmaxes[whichmaxes == 0] <- nbin
     # strength[i] <- sum(thedens$y[whichmaxes])/sum(thedens$y)
     # strengthvec <- thedens$y
+    
+    # Just binning at a regular interval
     thetab <- as.numeric(table(cut(folded, breaks=seq(from=min(folded), to=max(folded), length.out=nbin+1))))
     whichmaxes <- (which.max(thetab) + (-delta):delta) %% nbin
     whichmaxes[whichmaxes == 0] <- nbin
     strengthvec <- thetab
+    
+    # calculated "strength" of spectral peak
     strength[i] <- sum(strengthvec[whichmaxes])/sum(strengthvec)
+    
+    # approx corresponding p-value (NEEDS TO BE CORRECTED!!)
     pstrength[i] <- pbinom(q=sum(strengthvec[whichmaxes]), 
                            size=length(x),
                            prob=(1+(2*delta))/nbin,
                            lower.tail=FALSE)
   }
+  
+  # strength that would be "significant" at family-wise Sidak level
   strength_sidak <- qbinom(p=alpha_sidak,
                            size=length(x),
                            prob=(1+(2*delta))/nbin,
                            lower.tail=FALSE)/sum(strengthvec)
+  
   return(list(lam_trial=lam_trial, 
               strength=strength, 
               pval=pstrength,
@@ -160,9 +208,13 @@ spectra <- function(x, lam_min=1, lam_max=120, n_lam=1000, log_scale=TRUE) { #, 
               strength_sidak=strength_sidak))
 }
 
+# this will be used to explore additional spectral peaks at rational multiples of 3 seconds
 thegrid <- expand.grid(1:4, 1:4)
 thefrac <- thegrid[,1]/thegrid[,2]
 
+
+
+### plotting spectral peaks for tags with n>50 as identified earlier!
 par(mfrow=c(3,3))
 for(i in 1:length(datetime_secs_sub)) {
   thespectra <- spectra(x=datetime_secs_sub[[i]], 
